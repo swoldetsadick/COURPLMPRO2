3e8c7ec06b6bb9174c6606d6d4f4024d63c0c64e
swoldetsadick <swoldetsadick@ineteconomics.org>
2015-05-24 21:45:27 +0200

    ME

---
+++ bench/pml-files/jobs/01/1.R
@@ -0,0 +1,13 @@
+Sys.sleep(0.000000)
+options(BatchJobs.on.slave = TRUE, BatchJobs.resources.path = 'C:/Users/swl/Desktop/pml/bench/pml-files/resources/resources_1432472517.RData')
+library(checkmate)
+library(BatchJobs)
+res = BatchJobs:::doJob(
+	reg = loadRegistry('C:/Users/swl/Desktop/pml/bench/pml-files'),
+	ids = c(1L),
+	multiple.result.files = FALSE,
+	disable.mail = FALSE,
+	first = 1L,
+	last = 18L,
+	array.id = NA)
+BatchJobs:::setOnSlave(FALSE)
